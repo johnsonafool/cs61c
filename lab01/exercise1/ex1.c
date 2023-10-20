3a906fc30a00bb3524753a2c1600eaf167bf0be7
johnsonafool <johnsonafool@gmail.com>
2023-10-20 13:30:35 +0800

    lab01 ex1

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -7,7 +7,7 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+    return 3;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
